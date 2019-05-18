c45bc895fde5228cd9d31b86d68a4d307ab0a1b8
Charlie Sperbeck <charlie.sperbeck@gmail.com>
2019-05-18 11:12:23 -0400

    re-enabled entrypoint

--- containers/wifi-connect/startup.sh
+++ containers/wifi-connect/startup.sh
@@ -2,12 +2,6 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-if $(nmcli | grep -q 'wlan0: disconnected')
-    then
-        printf 'Starting WiFi Connect\n'
-        ./wifi-connect
-    else
-        printf 'Skipping WiFi Connect\n'    
-fi
+#if $(nmcli | grep -q 'wlan0: disconnected'); then printf 'Starting WiFi Connect\n' && ./wifi-connect; else printf 'Skipping WiFi Connect\n'; fi
 # Start your application here.
 sleep 60s
