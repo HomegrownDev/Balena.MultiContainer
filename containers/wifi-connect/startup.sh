ecaf1929184487459bc254c184fe195de9d859bc
Charlie Sperbeck <charlie.sperbeck@gmail.com>
2019-05-18 10:54:12 -0400

    changed entrypoint script

--- containers/wifi-connect/startup.sh
+++ containers/wifi-connect/startup.sh
@@ -2,15 +2,11 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-#Get current and active wifi connection
-iwgetid -r
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
+if [ -Z $(nmcli | grep -q 'wlan0: disconnected') ]; then
     printf 'Starting WiFi Connect\n'
     ./wifi-connect
+else
+    printf 'Skipping WiFi Connect\n'    
 fi
-
 # Start your application here.
 sleep 60s
