64f2fd3866446827a4c293f7d3484c3ea3f91aab
Charlie Sperbeck <charlie.sperbeck@gmail.com>
2019-05-18 11:00:03 -0400

    added specific interface for wlan0 in rPI

--- containers/wifi-connect/startup.sh
+++ containers/wifi-connect/startup.sh
@@ -2,11 +2,12 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-if [ -Z $(nmcli | grep -q 'wlan0: disconnected') ]; then
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect
-else
-    printf 'Skipping WiFi Connect\n'    
+if $(nmcli | grep -q 'wlan0: disconnected')
+    then
+        printf 'Starting WiFi Connect\n'
+        ./wifi-connect
+    else
+        printf 'Skipping WiFi Connect\n'    
 fi
 # Start your application here.
 sleep 60s
